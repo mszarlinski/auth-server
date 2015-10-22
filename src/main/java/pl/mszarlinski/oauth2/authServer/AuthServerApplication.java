8f2be48ac959bd6c02c63ceda12c9d27fcacb4b8
Maciej Szarlinski <mszarlinski@gmail.com>
2015-10-22 21:42:05 +0200

    Example of CURL commands

--- src/main/java/pl/mszarlinski/oauth2/authServer/AuthServerApplication.java
+++ src/main/java/pl/mszarlinski/oauth2/authServer/AuthServerApplication.java
@@ -6,21 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  REQUEST TO OBTAIN A TOKEN:
 
- curl --request POST \
- --url http://localhost:9999/auth-server/oauth/token \
- --header 'authorization: Basic cmVzb3VyY2Utc2VydmVyOnNlY3JldA==' \
- --header 'cache-control: no-cache' \
- --header 'content-type: application/x-www-form-urlencoded' \
- --header 'postman-token: fc77357c-28bd-3ded-750b-5dc3bbfd0e81' \
- --data 'username=mszarl&password=secret&client_id=resource-server&client_secret=&grant_type=password'
+ curl -X POST -H "Authorization: Basic cmVzb3VyY2Utc2VydmVyOnNlY3JldA=="
+ -H "Cache-Control: no-cache" -H "Postman-Token: 2b8fe5ee-ec3f-fc53-5ea8-e19b6ca4c489"
+ -H "Content-Type: application/x-www-form-urlencoded"
+ -d 'username=mszarl&password=secret&client_id=resource-server&client_secret=&grant_type=password&'
+ 'http://localhost:9999/auth-server/oauth/token'
 
  REQUEST TO CHECK A TOKEN:
 
- curl --request GET \
- --url 'http://localhost:9999/auth-server/oauth/check_token?token=536e65e1-ab87-4b0e-9cd6-1f7e43f7dd1c' \
- --header 'authorization: Basic cmVzb3VyY2Utc2VydmVyOnNlY3JldA==' \
- --header 'cache-control: no-cache' \
- --header 'postman-token: 5eb95cb0-0daf-d4b2-143f-27960c06d978'
+ curl -X POST -H "Authorization: Basic cmVzb3VyY2Utc2VydmVyOnNlY3JldA=="
+ -H "Cache-Control: no-cache" -H "Postman-Token: 8380b2c4-8c65-2294-7a38-dd9388c9f953"
+ 'http://localhost:9999/auth-server/oauth/check_token?token=c159efe2-4a2a-43d0-a779-c9b2a917f673'
  */
 @SpringBootApplication
 public class AuthServerApplication {
